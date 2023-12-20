9162e88b2cf810f1cb18bb0de264fe2ced9c1ed8
Serdar Şarbak <serdarsarbak@gmail.com>
2023-12-19 22:39:22 -0500

    Palindrom sayı

--- PalindromSayi/src/Main.java
+++ PalindromSayi/src/Main.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
 public class Main {
+    static boolean isPalindrom (int number) {
+
+        int tempNum, lastDigit, newNum = 0;
+        tempNum = number;
+        while (tempNum > 0) {
+
+            lastDigit = tempNum % 10;
+            newNum = newNum * 10 + lastDigit;
+
+            tempNum = tempNum / 10;
+
+        }
+
+        return number == newNum;
+    }
+
+
+    public static void main(String[] args) {
+        boolean isExit = false;
+        Scanner input = new Scanner (System.in);
+
+        while (!isExit ) {
+            System.out.print("Lütfen sayı giriniz: ");
+            int num = input.nextInt();
+
+            if (isPalindrom(num)) {
+                System.out.println(num + " sayısı palindrom sayıdır.");
+                isExit = true;
+            } else {
+                System.out.println(num + " sayısı palindrom sayı değildir. ");
+            }
+        }
+
+
+
+
+    }
 }
