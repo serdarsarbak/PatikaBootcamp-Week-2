9328153c4ef52dfae32e90e7689c8097c9fbb4ac
Serdar Şarbak <serdarsarbak@gmail.com>
2023-12-20 01:02:48 -0500

    Asal sayı

---
+++ AsalSayi/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    //Java dilinde "Recursive" metot kullanarak, kullanıcıdan alınan sayının "Asal" sayı olup olmadığını bulan program.
+    static boolean isPrimeNum (int num, int div) {
+        // negatif değer girilirse hata verme.
+        if (num<0) {
+            System.out.println("Pozitif bir sayı giriniz.");
+            System.exit(0);
+        }
+
+        // girilen değer 1 veya 0 ise asal sayı değildir.
+        if (num ==1 || num==0) {
+            return false;
+        }
+
+        //div eğer num sayısına kadar ulaşırsa num asal sayıdır.
+        if (num == div) {
+            return true;
+        }
+        // num div'e eşit bölünürse asal sayı değildir.
+        if (num % div==0) {
+            return false;
+        }
+        // recursive oluşumu; div 2 den başlayarak num sayısına kadar artar.
+        return isPrimeNum(num, div+1);
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        boolean isExit = false;
+
+        //asal sayı girene kadar sayı sormaya devam eder.
+        while (isExit == false) {
+
+            System.out.print("Lütfen bir sayı giriniz: ");
+            int num = input.nextInt();
+            if (isPrimeNum(num, 2)) {
+                System.out.println(num + " bir asal sayıdır.");
+                isExit = true;
+            } else {
+                System.out.println(num + " bir asal sayı değildir.");
+            }
+
+        }
+    }
+
+}
