33469ca40dfc91dbb8ae54c778ac998bac822bdf
Serdar Şarbak <serdarsarbak@gmail.com>
2023-12-21 22:39:52 -0500

    Boks maçı

--- BoksMaci/src/Main.java
+++ BoksMaci/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        // boks maçına başlayan dövüşçünün rasgele belirlenmesi yapılmıştır.
+
         Fighter marc = new Fighter("Marc" , 15 , 100, 90, 64);
         Fighter alex = new Fighter("Alex" , 10 , 95, 100, 45);
         Ring r = new Ring(marc,alex , 90 , 100);
